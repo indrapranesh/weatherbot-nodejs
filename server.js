8ff71ab08a716a5410ab4f14501b6f545a0fd670
indrapranesh <indrapranesh2111@gmail.com>
2019-04-14 21:05:25 +0530

    Create API for getting the weather of a city

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -7,32 +6,25 @@ const app = express()
 const apiKey = 'f3b2ffb736ee760ee123e66ba068c45e';
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
+  request(url, function (err,response,body){
+    
       let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
+       // console.log(weather.main.temp); 
+        res.json(weather);
+        
+        
 })
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example   app listening on port 3000!')
 })
