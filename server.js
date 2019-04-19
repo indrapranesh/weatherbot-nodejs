c65e20fc7b7b64228b226844e9cabca28a90e795
indrapranesh <indrapranesh2111@gmail.com>
2019-04-19 09:40:51 +0530

    Remove 3000 port function

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.post('/', function (req, res) {
 })
 });
 
-app.listen(3000, function () {
-  console.log('Example   app listening on port 3000!')
-})
+//app.listen(3000, function () {
+  //console.log('Example   app listening on port 3000!')
+//})
